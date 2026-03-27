03c5933a007f8830d395439ad5ec27258b58df53
vedantgade555 <vedantgade5555@gmail.com>
2026-03-27 23:31:46 +0530

    .

---
+++ Advance Java/SpringBoot/Spring Boot Security/form-base-and-session-base/src/main/java/com/vedant/form_base_and_session_base/FormBaseAndSessionBaseApplication.java
@@ -0,0 +1,13 @@
+package com.vedant.form_base_and_session_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBaseAndSessionBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormBaseAndSessionBaseApplication.class, args);
+	}
+
+}
